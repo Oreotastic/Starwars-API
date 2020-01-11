b429c323f86f94d1554f1aa34a24c2c30c666a92
Oretastic <hloreair@outlook.com>
2020-01-11 13:00:27 -0500

    added comments & formatted

--- app.js
+++ app.js
@@ -25,23 +25,34 @@ const vehiclesUrl = axios.get('https://star-cors.herokuapp.com/vehicles')
 
 const allPromise = Promise.all([filmsUrl, peopleUrl, planetsUrl, speciesUrl, starshipsUrl, vehiclesUrl])
 
-const grabApi = () => {
-    return allPromise
-        .then(response => response)
-}
+//Functions for Rendering Data
+//-----------------------------
 
+//Renders initial data when first loaded as well as future data if requested
 const renderMain = (data, count, id, page) => {
-
+    //data = data grabbed from api
+    //count = total amount of items in each chunk of data
+    //id = determines weather your grabbing 'films', 'people', 'planets', etc...
+    //page = current 'page' of data you're on
+    
+    //grabs dom elements that will contain each corresponding chunk of data
     const domArr = [films, people, planets, species, starships, vehicles]
-
+    
+    //if count is an array, it means the page is going through its initial load up
     if (typeof count === 'object') {
+
+        //each page of data is at most 10, so load each item thats inside this 'page' of data
         for (i = 0; i < 11; i++) {
 
+            //for each object in the data set, render the neccessary template box
             for (j in data[i]) {
 
                 const headerArr = ['films', 'people', 'planets', 'species', 'starships', 'vehicles']
+
+                //grab the first key and its value
                 const name = data[i][j][`${Object.keys(data[i][j])[0]}`]
 
+                //renders data 
                 domArr[i].innerHTML +=
                 `<li class="card dataset ${headerArr[i]}Title page${page}">
                 <b class="${headerArr[i]}Name">${name}</b>
@@ -60,7 +71,7 @@ const renderMain = (data, count, id, page) => {
     } else {
 
         const domArr = [films, people, planets, species, starships, vehicles]
-        
+
         switch (id) {
             case 'people':
                 peoplePage++
@@ -68,7 +79,7 @@ const renderMain = (data, count, id, page) => {
                 for (i in data) {
                     const name = data[i][`${Object.keys(data[i])[0]}`]
                     el.innerHTML +=
-                    `<li class="card dataset ${id}Title page${peoplePage}">
+                        `<li class="card dataset ${id}Title page${peoplePage}">
                     <b class="${id}Name">${name}</b>
                     </li>`
                 }
@@ -97,8 +108,8 @@ const renderMain = (data, count, id, page) => {
                     const name = data[i][`${Object.keys(data[i])[0]}`]
                     el.innerHTML +=
                         `<li class="card dataset ${id}Title page${speciesPage}">
-                        <b class="${id}Name">${name}</b>
-                        </li>`
+                            <b class="${id}Name">${name}</b>
+                            </li>`
                 }
 
                 renderSpecies(data, count, speciesPage)
@@ -111,8 +122,8 @@ const renderMain = (data, count, id, page) => {
                     const name = data[i][`${Object.keys(data[i])[0]}`]
                     el.innerHTML +=
                         `<li class="card dataset ${id}Title page${starshipsPage}">
-                        <b class="${id}Name">${name}</b>
-                        </li>`
+                                <b class="${id}Name">${name}</b>
+                                </li>`
                 }
 
                 renderStarships(data, count, starshipsPage)
@@ -124,9 +135,9 @@ const renderMain = (data, count, id, page) => {
                 for (i in data) {
                     const name = data[i][`${Object.keys(data[i])[0]}`]
                     el.innerHTML +=
-                    `<li class="card dataset${id}Title page${vehiclesPage}">
-                    <b class="${id}Name">${name}</b>
-                    </li>`
+                        `<li class="card dataset${id}Title page${vehiclesPage}">
+                                    <b class="${id}Name">${name}</b>
+                                    </li>`
                 }
 
                 renderVehicles(data, count, vehiclesPage)
@@ -148,11 +159,11 @@ const renderFilms = (arr, totalCount) => {
     for (let i = 0; i < filmList.length; i++) {
         filmList[i].innerHTML +=
             `
-        <div class="box">
-            <p>Episode: ${arr[i].episode_id}</p>
-            <p>Release Date: ${arr[i].release_date}</p>
-        </div>
-        `
+                            <div class="box">
+                            <p>Episode: ${arr[i].episode_id}</p>
+                            <p>Release Date: ${arr[i].release_date}</p>
+                            </div>
+                            `
     }
 
     renderCount(countEl, filmList.length, totalCount)
@@ -166,12 +177,13 @@ const renderPeople = (arr, totalCount, page) => {
     const countEl = newList[0].parentNode.firstElementChild
 
     for (let i = 0; i < newList.length; i++) {
+
         newList[i].innerHTML +=
             `
-        <div class="box">
-        <p>Films Appeared: ${arr[i].films.length}</p>
-        </div>
-        `
+                            <div class="box">
+                            <p>Films Appeared: ${arr[i].films.length}</p>
+                            </div>
+                            `
     }
 
     renderCount(countEl, fullList, totalCount)
@@ -185,12 +197,12 @@ const renderPlanets = (arr, totalCount, page) => {
     const countEl = newList[0].parentNode.firstElementChild
 
     for (let i = 0; i < newList.length; i++) {
-        arr
+
         newList[i].innerHTML +=
-        `<div class="box">
-        <p>Films Appeared: ${arr[i].films.length}</p>
-        <p>Climate: ${arr[i].climate}</p>
-        </div>`
+            `<div class="box">
+                            <p>Films Appeared: ${arr[i].films.length}</p>
+                            <p>Climate: ${arr[i].climate}</p>
+                            </div>`
     }
 
     renderCount(countEl, fullList, totalCount)
@@ -204,11 +216,12 @@ const renderSpecies = (arr, totalCount, page) => {
     const countEl = newList[0].parentNode.firstElementChild
 
     for (let i = 0; i < newList.length; i++) {
+
         newList[i].innerHTML +=
-        `<div class="box">
-        <p>Films Appeared: ${arr[i].films.length}</p>
-        <p>Average Lifespan: ${arr[i].average_lifespan}</p>
-        </div>`
+            `<div class="box">
+                            <p>Films Appeared: ${arr[i].films.length}</p>
+                            <p>Average Lifespan: ${arr[i].average_lifespan}</p>
+                            </div>`
     }
 
     renderCount(countEl, fullList, totalCount)
@@ -222,11 +235,12 @@ const renderStarships = (arr, totalCount, page) => {
     const countEl = newList[0].parentNode.firstElementChild
 
     for (let i = 0; i < newList.length; i++) {
+
         newList[i].innerHTML +=
-        `<div class="box">
-        <p>Model: ${arr[i].model}</p>
-        <p>Manufacturer: ${arr[i].manufacturer}</p>
-        </div>`
+            `<div class="box">
+                            <p>Model: ${arr[i].model}</p>
+                            <p>Manufacturer: ${arr[i].manufacturer}</p>
+                            </div>`
     }
 
     renderCount(countEl, fullList, totalCount)
@@ -240,115 +254,113 @@ const renderVehicles = (arr, totalCount, page) => {
     const countEl = newList[0].parentNode.firstElementChild
 
     for (let i = 0; i < newList.length; i++) {
+
         newList[i].innerHTML +=
-        `<div class="box">
-        <p>Model: ${arr[i].model}</p>
-        <p>Manufacturer: ${arr[i].manufacturer}</p>
-        </div>`
+            `<div class="box">
+                            <p>Model: ${arr[i].model}</p>
+                            <p>Manufacturer: ${arr[i].manufacturer}</p>
+                            </div>`
     }
 
     renderCount(countEl, fullList, totalCount)
 }
 
+//Functions for Grabbing API Data
 //--------------------------------------------------
+const grabApi = () => {
+    return allPromise
+        .then(response => response)
+}
 
 const loadMoreAPI = (id) => {
 
     switch (id) {
+
         case 'people':
             axios.get(`https://star-cors.herokuapp.com/${id}/?page=${peoplePage}`)
                 .then(response => response)
-                .then(({data}) => data.next)
+                .then(({
+                    data
+                }) => data.next)
                 .then(url => {
                     axios.get(url)
                         .then(response => response)
-                        .then(({data}) => {
+                        .then(({
+                            data
+                        }) => {
                             renderMain(data.results, data.count, id)
                         })
                 })
             break;
+
         case 'planets':
             axios.get(`https://star-cors.herokuapp.com/${id}/?page=${planetsPage}`)
                 .then(response => response)
-                .then(({data}) => data.next)
+                .then(({
+                    data
+                }) => data.next)
                 .then(url => {
                     axios.get(url)
                         .then(response => response)
-                        .then(({data}) => {
+                        .then(({
+                            data
+                        }) => {
                             renderMain(data.results, data.count, id)
                         })
                 })
             break;
+
         case 'species':
             axios.get(`https://star-cors.herokuapp.com/${id}/?page=${speciesPage}`)
                 .then(response => response)
-                .then(({data}) => data.next)
+                .then(({
+                    data
+                }) => data.next)
                 .then(url => {
                     axios.get(url)
                         .then(response => response)
-                        .then(({data}) => {
+                        .then(({
+                            data
+                        }) => {
                             renderMain(data.results, data.count, id)
                         })
                 })
             break;
+
         case 'starships':
             axios.get(`https://star-cors.herokuapp.com/${id}/?page=${starshipsPage}`)
                 .then(response => response)
-                .then(({data}) => data.next)
+                .then(({
+                    data
+                }) => data.next)
                 .then(url => {
                     axios.get(url)
                         .then(response => response)
-                        .then(({data}) => {
+                        .then(({
+                            data
+                        }) => {
                             renderMain(data.results, data.count, id)
                         })
                 })
             break;
+
         case 'vehicles':
             axios.get(`https://star-cors.herokuapp.com/${id}/?page=${vehiclesPage}`)
                 .then(response => response)
-                .then(({data}) => data.next)
+                .then(({
+                    data
+                }) => data.next)
                 .then(url => {
                     axios.get(url)
                         .then(response => response)
-                        .then(({data}) => {
+                        .then(({
+                            data
+                        }) => {
                             renderMain(data.results, data.count, id)
                         })
                 })
     }
 }
-
-const loadMore = () => {
-    for (let i = 0; i < loadBtn.length; i++) {
-        loadBtn[i].addEventListener('click', (event) => {
-            const id = event.target.parentElement.nextElementSibling.id
-            loadMoreAPI(id)
-        })
-    }
-}
-
-const addFilters = () => {
-
-    for (let i = 0; i < filters.length; i++) {
-
-        filters[i].addEventListener('input', (event) => {
-            
-            const nameList = event.target.parentElement.parentElement.querySelectorAll('b')
-            const nameArr = [...nameList]
-
-            for (let j in nameArr) {
-                if (!(nameArr[j].innerText.indexOf(event.target.value) === 0)) {
-
-                    nameArr[j].parentElement.classList = ('card hide ')
-
-                } else {
-                    nameArr[j].parentElement.classList = ('card display dataset')
-                }
-            }
-        })
-    }
-
-}
-
 
 grabApi()
     .then(response => response)
@@ -364,6 +376,44 @@ grabApi()
 
         renderMain(resultArr, countArr, null, 1)
     })
+
+//Functions Adding Event Listeners
+//-----------------------------------
+const loadMore = () => {
+
+    for (let i = 0; i < loadBtn.length; i++) {
+
+        loadBtn[i].addEventListener('click', (event) => {
+            const id = event.target.parentElement.nextElementSibling.id
+            loadMoreAPI(id)
+        })
+    }
+}
+
+
+
+const addFilters = () => {
+
+    for (let i = 0; i < filters.length; i++) {
+
+        filters[i].addEventListener('input', (event) => {
+
+            const nameList = event.target.parentElement.parentElement.querySelectorAll('b')
+            const nameArr = [...nameList]
+
+            for (let j in nameArr) {
+
+                if (!(nameArr[j].innerText.indexOf(event.target.value) === 0)) {
+                    nameArr[j].parentElement.classList = ('card hide ')
+
+                } else {
+                    nameArr[j].parentElement.classList = ('card display dataset')
+                }
+            }
+        })
+    }
+
+}
 
 
 addFilters()
